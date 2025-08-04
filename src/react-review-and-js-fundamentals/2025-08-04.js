6bd80636d5da2ce2fa3ccdc34ae55498e40941a6
Alex Walker <walkernalex@gmail.com>
2025-08-04 18:03:26 -0400

    Initial commit: React review practice files

---
+++ src/react-review-and-js-fundamentals/2025-08-04.js
@@ -0,0 +1,47 @@
+// --------------------
+// Lesson 1
+
+let date = "Monday";
+console.log(date);
+// date can be changed
+const startDate = "Monday";
+console.log(startDate);
+// date cannot be changed
+
+// --------------------
+// Lesson 2
+
+//Booleans
+const lightMode = true;
+const isLoggedIn = false;
+console.log("Light Mode:", lightMode);
+
+// Strings
+const username = "Alex";
+console.log("Hello, " + username);
+
+// Objects
+const user = {
+  name: "Alex",
+  age: 30,
+};
+console.log(user.name);
+
+// --------------------
+// Lesson 3
+// string-methods
+
+const faveSnacks = [chips, cookies, fruit, nuts];
+4;
+console.log(faveSnacks[2]);
+//fruits
+
+const faveBook = {
+  title: "Giovanni's Room",
+  author: "Baldwin",
+  pageNum: 134,
+  isRead: true,
+};
+
+console.log(favebook.title);
+//Giovanni's Room
